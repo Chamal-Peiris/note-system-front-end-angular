6dff4bb368303cc325d761b34859850f8d1ce952
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-06-24 14:12:20 +0530

    Add the basic prject structure and add material ui library

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NoteSystemFrontEndAngular
+# Note-System-Front-End-Angular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.7.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { NoteComponent } from './note/note.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
